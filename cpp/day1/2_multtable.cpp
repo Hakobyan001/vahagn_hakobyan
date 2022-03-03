94b80442bf0dcbba9ee057661c607aa5187fb6e9
vahagn_hakobyan <vahagn.hakobyan.2001@gmail.com>
2022-03-04 00:20:29 +0400

    Second Multtable

---
+++ cpp/day1/2_multtable.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout << "Print the number for Multiplication table \n";
+int n;
+int x = 1;
+cin >> n;
+        while (x <= 10){
+        cout << n << "*" << x << " = " << n * x << endl;
+        x++;
+        }return 0;
+}
+~               
