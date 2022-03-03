03e1b37dd6c079a053eb9b02cc1307a135e7662e
vahagn_hakobyan <vahagn.hakobyan.2001@gmail.com>
2022-03-04 00:27:58 +0400

    Fifth rectangle

---
+++ cpp/day1/5_rectangle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int erk, layn, mak, par;
+cout << "Print the length of a rectangle :\n";
+	cin >> layn;
+cout << "Print the width of the rectangle :\n ";
+        cin >> erk;
+mak = (erk * layn);
+par = 2 * (layn + erk);
+cout << " The Area of the rectangle is : " << mak << endl;
+cout << " The Perimeter of the rectangle is : " << par << endl;
+return 0;}
+    
+
