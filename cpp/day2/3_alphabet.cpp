52c5ed7aca70fe2a21e23dd303f84296509714c0
vahagn_hakobyan <vahagn.hakobyan.2001@gmail.com>
2022-03-05 20:51:33 +0400

    The Third has done

---
+++ cpp/day2/3_alphabet.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int ch;
+cout << "Print a character of alphabet \n";
+cin >> ch;
+    switch (ch)
+    {
+        case 'a': 
+            cout << ("Vowel \n");
+            break;
+        case 'i': 
+            cout << ("Vowel \n");
+            break;
+        case 'o': 
+            cout << ("Vowel \n");
+            break;
+        case 'u': 
+            cout << ("Vowel \n");
+            break;
+        case 'A': 
+            cout << ("Vowel \n");
+            break;
+        case 'I': 
+            cout << ("Vowel \n");
+            break;
+        case 'O': 
+            cout << ("Vowel \n");
+            break;
+        case 'U': 
+            cout << ("Vowel \n");
+            break;
+        case 'e': 
+            cout <<("Vowel \n");
+            break;
+        case 'E': 
+            cout << ("Vowel \n");
+            break;
+        default: 
+            cout << ("Consonant \n");
+    }
+    return 0;
+}
