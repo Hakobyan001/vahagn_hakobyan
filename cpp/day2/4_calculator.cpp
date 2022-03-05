7af8ce02c30180236a6edf29ec947060424977ef
vahagn_hakobyan <vahagn.hakobyan.2001@gmail.com>
2022-03-05 22:06:27 +0400

    The fourth has been installed

---
+++ cpp/day2/4_calculator.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+float num1;
+char function;
+float num2;
+cout << "Hello it's a Primitve Calculator.\n \n";
+cout << "Print your first number. \n";
+cin >> num1;
+cout << "Okay,now print the function/operator that will be done.( +, -, *, /\n";
+cin >> function;
+cout << "Good, and the finally print the second number. \n";
+cin >> num2;
+cout << "\nAnd the Result is \n";
+    switch (function) {
+	case '+': 
+ 	  cout << num1 + num2 << endl;
+	  break;
+	case '-':
+	  cout << num1 - num2 << endl;
+	  break;
+	case '*':
+	  cout << num1 * num2 << endl;
+	  break;
+	case '/':
+	  cout << num1 / num2 << endl;
+	  break;
+	default :
+	  cout << "Hmmm...It's Not an Operator/Number. Error. Try again. \n";	  
+	  break;
+	}
+return 0;
+}
