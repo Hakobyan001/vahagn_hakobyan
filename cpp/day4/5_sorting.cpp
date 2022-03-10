5fe4a884c95bb951d84db491077628be465c0414
vahagn_hakobyan <vahagn.hakobyan.2001@gmail.com>
2022-03-10 23:31:50 +0400

    final

--- cpp/day4/5_sorting.cpp
+++ cpp/day4/5_sorting.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
+#include <cstring>
 #include <time.h>
 using namespace std;
 
 int main() {
 srand (time(0));
-        int arr[6];
-	int arr1[6];
-        cout << "The Random Array is    \n ";
-	for (int i = 0; i < 6; i++) {
-        arr[i] = rand() % 50;
-	cout << arr[i] << "\n ";
+        int x;
+	cout << "Print the size of Array" << endl;
+	cin >> x;
+	int arr[x];
+        cout << " The Random Array is    \n";
+	for (int i = 0; i < x; i++) {
+        	arr[i] = rand() % 50;
+		cout << arr[i] << "  ";
         }
      	cout << "\n \n The Reverse Array is    \n";
-	for (int i = 5; i >= 0; i--) {
-	cout << arr[i] << endl;		
+		for (int i = x - 1; i >= 0; i--) {
+		cout << arr[i] << "  ";		
 	}
 	return 0;
 }
