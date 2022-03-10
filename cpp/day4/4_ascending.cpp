fdaf022b529c82633c8233c8bc447829edf56438
vahagn_hakobyan <vahagn.hakobyan.2001@gmail.com>
2022-03-10 23:32:22 +0400

    final

--- cpp/day4/4_ascending.cpp
+++ cpp/day4/4_ascending.cpp
@@ -4,28 +4,32 @@ using namespace std;
  
 int main() {
 srand (time(0));
-        int arr[6];
-        for (int i = 0; i < 6; i++) {
+	int x;
+	cout << "Print the size" << endl;
+	cin >> x ;
+        int arr[x];
+        cout << endl;
+	for (int i = 0; i < x; i++) {
         arr[i] = rand() % 50;
         cout << arr[i] << "  \n ";
         }
         cout << endl;
         int i,j,temp;
-	int size = 6;
+	int size = x;
 	for(i=0; i<size; i++)
-    {
-        for(j=i+1; j<size; j++)
-        {
-            if(arr[j] < arr[i])
-            {
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+    	{
+       		 for(j=i+1; j<size; j++)
+        	{
+            		if(arr[j] < arr[i])
+            		{
+              			temp = arr[i];
+                		arr[i] = arr[j];
+                		arr[j] = temp;
             }
         }
     }
     cout << "Elements of array in sorted ascending order:" << endl;
-    for(i=0; i<size; i++)
+    for (i=0; i < size; i++)
     {
         cout << arr[i] << endl;
     }
