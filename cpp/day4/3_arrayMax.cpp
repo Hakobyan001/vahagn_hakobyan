25607af506bb2e8ab9315895f2b3a7945804ce49
vahagn_hakobyan <vahagn.hakobyan.2001@gmail.com>
2022-03-10 23:33:57 +0400

    Final

--- cpp/day4/3_arrayMax.cpp
+++ cpp/day4/3_arrayMax.cpp
@@ -4,14 +4,17 @@ using namespace std;
 
 int main() {
 srand (time(0));
-	int arr[6];
-	for (int i = 0; i < 6; i++) {
+	int x;
+	cout << "Print the size \n";
+	cin >> x;
+	int arr[x];
+	for (int i = 0; i < x; i++) {
 	arr[i] = rand() % 100;
 	cout << arr[i] << "  \n ";
 	}
 	cout << endl;
 	int max = arr[0];
-	for (int j = 1; j < 7; j++) {
+	for (int j = 1; j < x; j++) {
 		if (arr[j] > max) {
 			max = arr[j];
 		} 
