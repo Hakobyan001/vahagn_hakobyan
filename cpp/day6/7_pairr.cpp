84d98d2a48cd21aa43eb83ffce39792a11cb4fb5
vahagn_hakobyan <vahagn.hakobyan.2001@gmail.com>
2022-03-14 02:52:25 +0400

    Final

--- cpp/day6/7_pairr.cpp
+++ cpp/day6/7_pairr.cpp
@@ -1,35 +1,31 @@
 #include <iostream>
 using namespace std;
 int main() {
-   const  int m = 4;
-   const  int n = 5;
+   const  int m = 4; 
+   const  int n = 2;
    int  matrix[m][n];
    int flag = 0;
+   int x = 0;
+   int y = 0;
    for (int i = 0; i < m; i++) {
        for (int j = 0; j < n; j++) {
-        	cin >> matrix[m][n];
+		matrix[i][j] = rand() % 30;
+		cout << matrix[i][j] << " ";
+		if ( matrix[i][j] % 2 == 0) {
+			flag = 1;
+			x = i;
+			y = j;
+		 	}	
+		else if( matrix[i][j] % 2 != 0) {
+			flag = 0;
+		} 
 	}
-    }
-   bool boolean = true;
-   for (int i = 0; i < m; i++) {
-       for (int j = 0; j < n; j++) {
-           if (matrix[i][j] % 2 == 0) {
-               flag++;
-           }
-       }
-       if (flag == n) {
-           cout << "The result is  ";
-           for (int k = 0; k < n; k++) {
-               cout << matrix[i][k] << " ";
-           }
-           boolean = false;
-           break;
-       }
-       flag = 0;
-   }
-   if (boolean) {
-       cout << "No";
-   }
-   cout << endl;
- return 0;
+	cout << endl;
+}
+	if( flag = 0 ){
+		cout << "No" << endl;
+		return 0;
+	}
+	cout << endl << " The Result is   " << matrix[x][y] << "'s pair"  << endl;
+	 return 0;
 }
