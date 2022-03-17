cd61acfea6d03fe6ec56c6d69ca5f52aea48f554
vahagn_hakobyan <vahagn.hakobyan.2001@gmail.com>
2022-03-18 00:45:28 +0400

    Ready

--- cpp/day10/1_sumcounting.cpp
+++ cpp/day10/1_sumcounting.cpp
@@ -13,13 +13,15 @@ int main (int argc, char** argv) {
 void counting(int amount, double percent, int month) {
 	time_t now = time(0);
 	int difference = 0;
+        int startsum = amount;
 	for (int j = 0; j < month; j++) {	
-                difference = amount * (percent / 100 / 12) ;
-                amount += amount * percent / 100;
+		difference = amount * (percent / 100 / 12) ;
+                amount += amount * percent / 100 / 12;
 		now += 2635200;
 		char* dt = ctime(&now);
 		cout << "      " << "     "  <<  amount  << "       " << difference << "    " << dt;
 		}
-	cout << "The Result is   " << amount << endl;
+	startsum = amount - startsum;
+	cout << "The Result is   " << amount << "     . And the income  is    "  << startsum << endl;
 }
 
