e15cb9100c8dbf1d97b9ee6c5bfd47c02369e97b
vahagn_hakobyan <vahagn.hakobyan.2001@gmail.com>
2022-03-18 00:04:45 +0400

    Ready

---
+++ cpp/day10/1_sumcounting.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void counting(int, double, int);
+int main (int argc, char** argv) {
+	int sum = atoi(argv[1]);
+        double perc = atof(argv[2]);
+        int month = atoi(argv[3]);
+	counting(sum, perc, month);
+        return 0;
+}
+void counting(int amount, double percent, int month) {
+	time_t now = time(0);
+	int difference = 0;
+	for (int j = 0; j < month; j++) {	
+                difference = amount * (percent / 100 / 12) ;
+                amount += amount * percent / 100;
+		now += 2635200;
+		char* dt = ctime(&now);
+		cout << "      " << "     "  <<  amount  << "       " << difference << "    " << dt;
+		}
+	cout << "The Result is   " << amount << endl;
+}
+
